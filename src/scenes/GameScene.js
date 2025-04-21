11686df9af1433bceead0e8141bca6c5864d1010
Kokeh <kokehinc@gmail.com>
2025-04-21 01:24:20 +0100

    add floorPhysics and image

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -26,8 +26,30 @@ export default class GameScene extends Phaser.Scene {
     bg.setPosition(width / 2, height / 2);
     bg.setOrigin(0.5, 0.5);
 
-    // this.ground = this.physics.add.staticGroup();
-    // this.ground.create(100, 280, 'ground').setScale(2).refreshBody();
+    // const floor = this.add.image(0, this.scale.height, 'arena-ground').setOrigin(0, 0);
+    // // Align left and bottom
+    // floor.setOrigin(0, 1);
+    // // Scale it to fit width
+    // floor.displayWidth = this.scale.width;
+    // floor.setScale(floor.scaleX, 0.1);
+
+    const floorHeight = 40;
+    // Visual layer
+    this.floorVisual = this.add.tileSprite(
+      0,
+      this.scale.height - floorHeight,
+      this.scale.width,
+      floorHeight,
+      'arena-ground'
+    ).setOrigin(0, 0);
+
+    // Collision layer
+    this.floorPhysics = this.physics.add.staticGroup();
+    this.floorPhysics.create(
+      this.scale.width / 2,
+      this.scale.height - 20,
+      'arena-ground'
+    ).setScale(1, 0.2).refreshBody();
 
     this.player = new Player(this, 100, 500);
     this.tokens = this.physics.add.group();
@@ -36,7 +58,7 @@ export default class GameScene extends Phaser.Scene {
     this.tokenTimer = this.time.addEvent({ delay: 1000, callback: () => new Token(this), loop: true });
     this.powerupTimer = this.time.addEvent({ delay: 10000, callback: () => spawnPowerup(this), loop: true });
 
-    this.physics.add.collider(this.player.sprite, this.ground);
+    this.physics.add.collider(this.player.sprite, this.floorPhysics);
     this.physics.add.overlap(this.player.sprite, this.tokens, (_, token) => {
       token.destroy();
       this.player.collectToken();
