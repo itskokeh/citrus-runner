191b56659448c27c6a813ae3897d39a8309dafe7
Kokeh <kokehinc@gmail.com>
2025-04-21 05:02:15 +0100

    changed token to factory function

--- src/objects/Token.js
+++ src/objects/Token.js
@@ -1,14 +1,9 @@
-export default class Token {
-  constructor (scene) {
-    this.scene = scene;
-    // Spawn token just outside the right side of the screen
-    const xPosition = this.scene.scale.width + 100;
-    const yPosition = Phaser.Math.Between(400, 500);
+export function spawnToken (scene) {
+  const xPosition = scene.scale.width + 100;
+  const yPosition = Phaser.Math.Between(400, 500);
 
-    const token = scene.tokens.create(xPosition, yPosition, 'token');
-    // Move token to the left at a constant speed
-    token.setVelocityX(-50);
-    token.setCollideWorldBounds(false);
-    token.setImmovable(true);
-  }
+  const token = scene.tokens.create(xPosition, yPosition, 'token');
+  token.setVelocityX(-50);
+  token.setCollideWorldBounds(false);
+  token.setImmovable(true);
 }
