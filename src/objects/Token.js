9b5108b5e2817749429af69a2a78cd47cb4429bc
Kokeh <kokehinc@gmail.com>
2025-04-21 04:30:42 +0100

    add token spawn positions

--- src/objects/Token.js
+++ src/objects/Token.js
@@ -1,9 +1,13 @@
 export default class Token {
   constructor (scene) {
     this.scene = scene;
+    // Spawn token just outside the right side of the screen
+    const xPosition = this.scene.scale.width + 100;
+    const yPosition = Phaser.Math.Between(400, 500);
 
-    const token = scene.tokens.create(800, Phaser.Math.Between(400, 550), 'token');
-    token.setVelocityX(-200);
+    const token = scene.tokens.create(xPosition, yPosition, 'token');
+    // Move token to the left at a constant speed
+    token.setVelocityX(-50);
     token.setCollideWorldBounds(false);
     token.setImmovable(true);
   }
