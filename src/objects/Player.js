a3ac94c63de1d939b261724c773b5026a9a949a5
Kokeh <kokehinc@gmail.com>
2025-04-21 13:10:57 +0100

    and double jump and suspension

--- src/objects/Player.js
+++ src/objects/Player.js
@@ -5,6 +5,10 @@ export default class Player {
     this.cursors = scene.input.keyboard.createCursorKeys();
     this.score = 0;
     scene.registry.set('score', 0);
+    this.maxJumps = 2;
+    this.jumpCount = 2;
+    this.airSuspension = false;
+    // this.sprite.body.setGravityY(1000);
   }
 
   collectToken () {
@@ -13,8 +17,32 @@ export default class Player {
   }
 
   update () {
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.space)) {
+      if (this.sprite.body.blocked.down || this.jumpCount < this.maxJumps) {
+        this.sprite.setVelocityY(-400);
+        this.jumpCount++;
+        //  If this is the second jump, extend hang time slightly
+        if (this.jumpCount === 2) {
+          this.temporarilyFloat();
+        }
+      }
+    }
     if (this.cursors.up.isDown && this.sprite.body.touching.down) {
+      this.jumpCount = 0;
       this.sprite.setVelocityY(-500);
     }
+  }
+
+  temporarilyFloat () {
+    if (this.airSuspension) return;
+
+    this.airSuspension = true;
+    const originalGravity = this.sprite.body.gravity.y;
+    this.sprite.body.setGravityY(500);
+
+    this.scene.time.delayedCall(800, () => {
+      this.sprite.body.setGravityY(originalGravity);
+      this.airSuspension = false;
+    });
   }
 }
