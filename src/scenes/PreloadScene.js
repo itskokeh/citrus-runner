41b2fae6945406eeaeaf2d4a8bd0ccdafb510f44
Kokeh <kokehinc@gmail.com>
2025-04-22 23:13:42 +0100

    preload new assets (arena, floor, obstacles, player

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -4,15 +4,23 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload () {
-    this.load.image('arena', '/arcade-arena.png');
-    this.load.image('arena-floor', '/arcade-floor.png');
-    this.load.image('arena-ground', '/arcade-floor-1.png');
-    this.load.spritesheet('player', 'assets');
+    this.load.image('arena-1', '/arcade-4.webp');
+    this.load.image('arena-2', '/arcade-2.webp');
+    this.load.image('arena-3', '/arcade-3.webp');
+    this.load.image('arena-4', '/arcade-4.webp');
+    // this.load.image('arena-wall', '/arcade-arena-02.png');
+    // this.load.image('arena-wall-top', '/arcade-arena-03.png');
+    // this.load.image('arena-floor', '/arcade-floor.png');
+    this.load.image('arena-ground', '/arcade-floor-top.png');
+    this.load.image('crate', 'assets');
+    this.load.image('spike', 'assets');
+    this.load.image('flying-enemy', 'assets');
     this.load.image('token', 'assets');
     this.load.image('magnet', 'assets');
     this.load.image('double', 'assets');
     this.load.image('speed', 'assets');
     this.load.image('shield', 'assets');
+    this.load.spritesheet('player', 'assets');
   }
 
   create () {
