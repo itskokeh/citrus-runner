852fed2b9ed2de18f174392a43f091f924a417ed
Kokeh <kokehinc@gmail.com>
2025-04-24 16:28:27 +0100

    set size of citrus tokens

--- src/objects/Token.js
+++ src/objects/Token.js
@@ -3,6 +3,8 @@ export function spawnToken (scene) {
   const yPosition = Phaser.Math.Between(400, 500);
 
   const token = scene.tokens.create(xPosition, yPosition, 'token');
+  token.displayWidth = 56;
+  token.displayHeight = 720 * (56 / 1320);
   token.setVelocityX(-50);
   token.setCollideWorldBounds(false);
   token.setImmovable(true);
