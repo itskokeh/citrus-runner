8bcabf431fabf88c987db27a4bd95757b14649f4
Kokeh <kokehinc@gmail.com>
2025-04-24 22:11:59 +0100

    remove unused assets

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -10,13 +10,10 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('arena-4', '/arcade-4.webp');
     this.load.image('arena-ground', '/arcade-floor-top-5.png');
     this.load.image('crate', 'assets');
-    this.load.image('spike', 'assets');
     this.load.image('flying-dynamite', '/flying-dynamite.webp');
     this.load.image('token', '/citrus-token.webp');
     this.load.image('magnet', 'assets');
     this.load.image('double', 'assets');
-    this.load.image('speed', 'assets');
-    this.load.image('shield', 'assets');
     this.load.spritesheet('player', 'assets');
     this.load.audio('backgroundMusic', '/Space track.mp3');
   }
