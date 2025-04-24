c2d1cf2f545bfb8a2ae2fb3843cc394ebcaa1fd3
Kokeh <kokehinc@gmail.com>
2025-04-24 16:27:21 +0100

    add citrus tokens and background music

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -8,19 +8,17 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('arena-2', '/arcade-2.webp');
     this.load.image('arena-3', '/arcade-3.webp');
     this.load.image('arena-4', '/arcade-4.webp');
-    // this.load.image('arena-wall', '/arcade-arena-02.png');
-    // this.load.image('arena-wall-top', '/arcade-arena-03.png');
-    // this.load.image('arena-floor', '/arcade-floor.png');
-    this.load.image('arena-ground', '/arcade-floor-top.png');
+    this.load.image('arena-ground', '/arcade-floor-top-5.png');
     this.load.image('crate', 'assets');
     this.load.image('spike', 'assets');
     this.load.image('flying-enemy', 'assets');
-    this.load.image('token', 'assets');
+    this.load.image('token', '/citrus-token.webp');
     this.load.image('magnet', 'assets');
     this.load.image('double', 'assets');
     this.load.image('speed', 'assets');
     this.load.image('shield', 'assets');
     this.load.spritesheet('player', 'assets');
+    this.load.audio('backgroundMusic', '/Space track.mp3');
   }
 
   create () {
