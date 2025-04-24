a526d486a14e96b2def672ba8d97f35657060206
Kokeh <kokehinc@gmail.com>
2025-04-24 22:21:31 +0100

    remove unused code

--- src/objects/obstacles/obstacleFactory.js
+++ src/objects/obstacles/obstacleFactory.js
@@ -1,5 +1,4 @@
 import createCrate from './crate';
-import createMovingSpike from './movingSpike';
 import createFlyingEnemy from './flyingEnemy';
 import createGroup from './groupFactory';
 
@@ -7,9 +6,7 @@ export function createObstacle (type, scene, x, y, options = {}) {
   switch (type) {
     case 'crate':
       return createCrate(scene, x, y, options);
-    case 'spike':
-      return createMovingSpike(scene, x, y, options);
-    case 'flying-enemy':
+    case 'flying-dynamite':
       return createFlyingEnemy(scene, x, y, options);
     case 'group':
       return createGroup(scene, x, y, options);
