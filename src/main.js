c47d2aed5a8e0da21eeb9dbb94f8ded0a2740f13
Kokeh <kokehinc@gmail.com>
2025-04-25 01:26:33 +0100

    add scale to fit all sceens

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import UIScene from './scenes/UIScene';
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
   backgroundColor: '#87ceeb',
   physics: {
     default: 'arcade',
@@ -14,7 +14,7 @@ const config = {
   },
   scene: [PreloadScene, GameScene, UIScene],
   scale: {
-    mode: Phaser.Scale.FIT,
+    mode: Phaser.Scale.RESIZE,
     autocenter: Phaser.Scale.CENTER_BOTH
   }
 };
