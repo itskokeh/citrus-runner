a8a5c0f26f393281fe5fc8cbc8ae166619716b52
Kokeh <kokehinc@gmail.com>
2025-04-26 19:27:10 +0100

    add flying-dynamites

--- src/objects/obstacles/flyingEnemy.js
+++ src/objects/obstacles/flyingEnemy.js
@@ -1,8 +1,12 @@
 export default function createFlyingEnemy (scene, x, y, options = {}) {
-  const enemy = scene.physics.add.image(x, y, 'flying-dynamite');
-  enemy.setVelocityX(options.speed || -300);
-  enemy.setSize(options.width || 48, options.height || 32);
+  const width = options.width || 48;
+  const height = options.height || 56;
 
-  scene.obstacles.add(enemy);
+  const enemy = scene.obstacles.create(x, y, 'flying-dynamite');
+  enemy.setVelocityX(options.speed || -300);
+  enemy.setSize(width, height);
+  enemy.setDisplaySize(width, height);
+  // enemy.body.allowGravity = false;
+
   return enemy;
 }
