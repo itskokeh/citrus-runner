509adf8b3cbb37a212b8a24d2dba1037f947e16e
Kokeh <kokehinc@gmail.com>
2025-04-26 17:49:31 +0100

    create crate and its logic

--- src/objects/obstacles/crate.js
+++ src/objects/obstacles/crate.js
@@ -1,8 +1,12 @@
 export default function createCrate (scene, x, y, options = {}) {
-  const crate = scene.physics.add.image(x, y, 'crate');
+  const width = options.width || 48;
+  const height = options.height || 48;
+
+  const crate = scene.obstacles.create(x, y, 'crate');
   crate.setImmovable(true);
   crate.setVelocityX(options.speed || -200);
-  crate.setSize(options.width || 32, options.height || 32);
-  scene.obstacles.add(crate);
+  crate.setSize(width, height);
+  crate.setDisplaySize(width, height);
+
   return crate;
 }
