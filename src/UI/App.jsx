159c8cb54f62856b84f325b17183b882714db861
Kokeh <kokehinc@gmail.com>
2025-05-02 00:41:16 +0100

    add react to project and clickable option to enable fullscrren and landscape mode for mobile screens

---
+++ src/UI/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+
+export default function App () {
+  const requestLandscape = async () => {
+    try {
+      if (screen.orientation?.lock) {
+        await screen.orientation.lock('landscape').catch(e => console.warn(e));
+      }
+      if (!document.fullscreenElement) {
+        await document.documentElement.requestFullscreen().catch(e => console.warn(e));
+      }
+    } catch (err) {
+      console.warn('could not lock orientation:', err);
+    }
+  };
+
+  useEffect(() => {
+    requestLandscape();
+  }, []);
+
+  return (
+    <div className='game-container'>
+      <button onClick={requestLandscape}>Enter Fullscreen (Landscape)</button>
+      <p>Just checking to see if it appears in landscape mode</p>
+      {/* {Game Content} */}
+    </div>
+  );
+}
