880fa85d666438f552e712e2264414f7b20e2c46
Kokeh <kokehinc@gmail.com>
2025-05-02 01:26:53 +0100

    testing landscape and return portrait mode

--- src/UI/App.jsx
+++ src/UI/App.jsx
@@ -1,27 +1,43 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 export default function App () {
+  const [isLandscape, setIsLandscape] = useState(false);
+
   const requestLandscape = async () => {
     try {
       if (!document.fullscreenElement) {
-        await document.documentElement.requestFullscreen().catch(e => console.warn(e));
+        await document.documentElement.requestFullscreen();
       }
       if (screen.orientation?.lock) {
-        await screen.orientation.lock('landscape').catch(e => console.warn(e));
+        await screen.orientation.lock('landscape');
+        setIsLandscape(true);
       }
     } catch (err) {
       console.warn('could not lock orientation:', err);
     }
   };
 
-  useEffect(() => {
-    requestLandscape();
-  }, []);
+  const returnToPortrait = async () => {
+    try {
+      if (screen.orientation?.unlock) {
+        await screen.orientation.unlock();
+        setIsLandscape(false);
+      }
+      if (document.fullscreenElement) {
+        await document.exitFullscreen();
+      }
+    } catch (err) {
+      console.warn('Error returning to portrait:', err);
+    }
+  };
 
   return (
     <div className='game-container'>
-      <button onClick={requestLandscape}>Enter Fullscreen (Landscape)</button>
+      {!isLandscape
+        ? (<button onClick={requestLandscape}>Enter Game</button>)
+        : (<button onClick={returnToPortrait}>Exit to Home</button>)};
+      {/* <button onClick={requestLandscape}>Enter Fullscreen (Landscape)</button> */}
       <p>Just checking to see if it appears in landscape mode</p>
       {/* {Game Content} */}
     </div>
