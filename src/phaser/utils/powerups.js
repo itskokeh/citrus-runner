1d01a2a750ea4948225fd26d10f720a5298a63e2
Kokeh <kokehinc@gmail.com>
2025-05-05 19:13:04 +0100

    rewrite powerups logic

--- src/phaser/utils/powerups.js
+++ src/phaser/utils/powerups.js
@@ -1,66 +1,34 @@
-// export function spawnPowerup (scene) {
-//   const types = ['magnet', 'double', 'speed', 'shield'];
-//   const type = Phaser.Utils.Array.GetRandom(types);
-//   const powerup = scene.powerups.create(800, Phaser.Math.Between(300, 500), type);
-//   powerup.setVelocityX(-200);
-// }
+import { doubleCoins } from '../objects/DoubleCoins';
+import { magnet } from '../objects/Magnet';
 
-// export function applyPowerup (player, type) {
-//   switch (type) {
-//     case 'magnet':
-
-//       break;
-//     case 'double':
-//       player.doubleCoins = true;
-//       break;
-//     case 'speed':
-//       player.sprite.setVelocityX(100);
-//       break;
-//     case 'shield':
-//       player.shielded = true;
-//       break;
-//   }
-//   setTimeout(() => {
-//     if (type === 'double') player.doubleCoins = false;
-//     if (type === 'speed') player.sprite.setVelocityX(0);
-//     if (type === 'shield') player.shielded = false;
-//   }, 15000);
-// }
-
-import { scaleObjectToScreen, scaleValue } from '../utils/scaleObject';
+const powerupFunctions = [magnet, doubleCoins];
 
 export function spawnPowerup (scene) {
-  const baseScale = scaleObjectToScreen(scene.scale.width, scene.scale.height, scene, 1);
-  const types = ['magnet', 'double', 'speed', 'shield'];
-  const type = Phaser.Utils.Array.GetRandom(types);
-  const powerup = scene.powerups.create(
-    scaleValue(800, baseScale),
-    scaleValue(Phaser.Math.Between(300, 500), baseScale),
-    type
-  );
-  // Scale powerup size
-  scaleObjectToScreen(scene.scale.width, scene.scale.height, powerup, 48 / 1280);
-  powerup.setVelocityX(scaleValue(-200, baseScale));
+  console.log('attempting to spwan powerup');
+  const randomPowerup = Phaser.Utils.Array.GetRandom(powerupFunctions);
+  randomPowerup(scene);
 }
 
 export function applyPowerup (player, type) {
+  if (player[`${type}Tween`]) {
+    player[`${type}Tween`].remove();
+  }
+
   switch (type) {
     case 'magnet':
+      player.hasMagnet = true;
       break;
-    case 'double':
+    case 'double-coin':
       player.doubleCoins = true;
       break;
-    case 'speed':
-    { const baseScale = scaleObjectToScreen(player.scene.scale.width, player.scene.scale.height, player, 1);
-      player.sprite.setVelocityX(scaleValue(100, baseScale));
-      break; }
-    case 'shield':
-      player.shielded = true;
-      break;
   }
-  setTimeout(() => {
-    if (type === 'double') player.doubleCoins = false;
-    if (type === 'speed') player.sprite.setVelocityX(0);
-    if (type === 'shield') player.shielded = false;
-  }, 15000);
+
+  player[`${type}Tween`] = player.scene.tweens.addCounter({
+    duration: 15000,
+    onComplete: () => {
+      if (type === 'magnet') player.hasMagnet = false;
+      if (type === 'double-coin') player.doubleCoins = false;
+      player[`${type}Tween`] = null;
+    }
+  });
 }
