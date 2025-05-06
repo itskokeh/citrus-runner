96514c13e1df79205569f1bc14384eac24b0520a
Kokeh <kokehinc@gmail.com>
2025-05-06 17:05:00 +0100

    create powerups folder and add spawner to it

---
+++ src/phaser/objects/powerups/spawner.js
@@ -0,0 +1,34 @@
+import { doubleCoins } from './DoubleCoins';
+import { magnet } from './Magnet';
+
+const powerupFunctions = [magnet, doubleCoins];
+
+export function spawnPowerup (scene) {
+  console.log('attempting to spwan powerup');
+  const randomPowerup = Phaser.Utils.Array.GetRandom(powerupFunctions);
+  randomPowerup(scene);
+}
+
+// export function applyPowerup (player, type) {
+//   if (player[`${type}Tween`]) {
+//     player[`${type}Tween`].remove();
+//   }
+
+//   switch (type) {
+//     case 'magnet':
+//       player.hasMagnet = true;
+//       break;
+//     case 'double-coin':
+//       player.doubleCoins = true;
+//       break;
+//   }
+
+//   player[`${type}Tween`] = player.scene.tweens.addCounter({
+//     duration: 15000,
+//     onComplete: () => {
+//       if (type === 'magnet') player.hasMagnet = false;
+//       if (type === 'double-coin') player.doubleCoins = false;
+//       player[`${type}Tween`] = null;
+//     }
+//   });
+// }
