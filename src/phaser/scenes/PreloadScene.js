557150d5903f696d330bad8dc551c31d89b9554c
Kokeh <kokehinc@gmail.com>
2025-05-06 16:55:12 +0100

    rename and rearrange assets

--- src/phaser/scenes/PreloadScene.js
+++ src/phaser/scenes/PreloadScene.js
@@ -8,12 +8,12 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('arena-2', '/arcade-2.webp');
     this.load.image('arena-3', '/arcade-3.webp');
     this.load.image('arena-4', '/arcade-4.webp');
-    this.load.image('arena-ground', '/arcade-floor-top-5.png');
+    // this.load.image('arena-ground', '/arcade-floor-top-5.png');
     this.load.image('crate', '/obstacles/crate.webp');
     this.load.image('flying-dynamite', '/obstacles/flying-dynamite.webp');
     this.load.image('magnet', '/powerups/magnet.webp');
-    this.load.image('double', '/powerups/double-coin.webp');
-    this.load.image('token', '/citrus-token.webp');
+    this.load.image('double-coin', '/powerups/double-coin.webp');
+    this.load.image('token', '/token/citrus-token.webp');
     this.load.spritesheet('player', 'assets');
     // this.load.audio('backgroundMusic', '/Space track.mp3');
   }
