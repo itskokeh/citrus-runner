1931e34c64108bed28418d6d9109821dc1fb3325
Kokeh <kokehinc@gmail.com>
2025-05-08 15:07:05 +0100

    add emptyOutDir to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()], // Only needed for React components
+  plugins: [react()],
 
   build: {
-    // Disable automatic directory cleaning (safety first!)
-    emptyOutDir: false,
+    emptyOutDir: true,
 
     rollupOptions: {
       // Multi-page setup
       input: {
-        main: path.resolve(__dirname, './index.html'), // React app
-        game: path.resolve(__dirname, './src/phaser/main.js') // Phaser entry
+        main: path.resolve(__dirname, './index.html'),
+        game: path.resolve(__dirname, './src/phaser/main.js')
       },
       output: {
         // Keep builds separated
