4fa4e8311a0104603c34c36d46e2bde67717198d
Kokeh <kokehinc@gmail.com>
2025-05-08 17:28:59 +0100

    install react 18

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  },
 
   build: {
     emptyOutDir: true,
@@ -19,6 +22,7 @@ export default defineConfig({
         entryFileNames: '[name]/[name]-[hash].js',
         assetFileNames: '[name]/assets/[name]-[hash].[ext]'
       }
-    }
+    },
+    sourcemap: false,
   }
 });
