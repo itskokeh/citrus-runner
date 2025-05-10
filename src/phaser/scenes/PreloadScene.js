93ad90b713588a7dec12d64423734b788343183e
Kokeh <kokehinc@gmail.com>
2025-05-10 18:37:21 +0100

    add backgrounds and player sprite

--- src/phaser/scenes/PreloadScene.js
+++ src/phaser/scenes/PreloadScene.js
@@ -9,17 +9,16 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('play-icon', '/icons/play_24dp_FFF.png');
     this.load.image('mute-icon', '/icons/volume_off_24dp_FFF.png');
     this.load.image('unmute-icon', '/icons/volume_up_24dp_FFF.png');
-    this.load.image('arena-1', '/arcade-1.webp');
-    this.load.image('arena-2', '/arcade-2.webp');
-    this.load.image('arena-3', '/arcade-3.webp');
-    this.load.image('arena-4', '/arcade-4.webp');
-    // this.load.image('arena-ground', '/arcade-floor-top-5.png');
+    this.load.image('arena-1', '/backgrounds/arcade-1.webp');
+    this.load.image('arena-2', '/backgrounds/arcade-2.webp');
+    this.load.image('arena-3', '/backgrounds/arcade-3.webp');
+    this.load.image('arena-4', '/backgrounds/arcade-4.webp');
     this.load.image('crate', '/obstacles/crate.webp');
     this.load.image('flying-dynamite', '/obstacles/flying-dynamite.webp');
     this.load.image('magnet', '/powerups/magnet.webp');
     this.load.image('double-coin', '/powerups/double-coin.webp');
     this.load.image('token', '/token/citrus-token.webp');
-    this.load.spritesheet('player', 'assets');
+    this.load.image('player', '/player-sprite/playersprite.webp');
     // this.load.audio('backgroundMusic', '/Space track.mp3');
   }
 
