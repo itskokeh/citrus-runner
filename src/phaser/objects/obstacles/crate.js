873e7f039ba0d6f1eb52a26ccb5021ee216c59e2
Kokeh <kokehinc@gmail.com>
2025-05-11 00:49:07 +0100

    add physics to dynamite and crate

--- src/phaser/objects/obstacles/crate.js
+++ src/phaser/objects/obstacles/crate.js
@@ -1,12 +1,13 @@
 export default function createCrate (scene, x, y) {
-  const crate = scene.obstacles.create(x, y, 'crate');
+  const crate = scene.physics.add.image(x, y, 'crate');
+  scene.obstacles.add(crate);
   const targetWidth = 50;
   const targetHeight = targetWidth * (crate.height / crate.width);
 
   crate.setImmovable(true);
   crate.setVelocityX(-scene.gameSpeed * scene.objSpeed);
-  crate.setSize(targetWidth, targetHeight);
   crate.setDisplaySize(targetWidth, targetHeight);
+  crate.body.setAllowGravity(false);
 
   return crate;
 }
--- src/phaser/objects/obstacles/flyingDynamite.js
+++ src/phaser/objects/obstacles/flyingDynamite.js
@@ -1,11 +1,12 @@
 export default function createFlyingEnemy (scene, x, y) {
-  const enemy = scene.obstacles.create(x, y, 'flying-dynamite');
+  const enemy = scene.physics.add.image(x, y, 'flying-dynamite');
+  scene.obstacles.add(enemy);
   const targetWidth = 100;
   const targetHeight = targetWidth * (enemy.height / enemy.width);
 
   enemy.setVelocityX(-scene.gameSpeed * scene.objSpeed);
-  enemy.setSize(targetWidth, targetHeight);
   enemy.setDisplaySize(targetWidth, targetHeight);
+  enemy.body.setAllowGravity(false);
 
   return enemy;
 }
