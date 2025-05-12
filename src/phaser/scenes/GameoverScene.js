e924b2bd4fe8a7360c04fbbf7c1276335a5a4b5b
Kokeh <kokehinc@gmail.com>
2025-05-12 08:49:47 +0100

    add smooth transition to gameOver screen

--- src/phaser/scenes/GameoverScene.js
+++ src/phaser/scenes/GameoverScene.js
@@ -6,15 +6,15 @@ export default class GameOverScene extends Phaser.Scene {
   create () {
     const { width, height } = this.sys.game.canvas;
 
-    this.add.rectangle(0, 0, width, height, 0x000000, 0.6).setOrigin(0);
+    const overlay = this.add.rectangle(0, 0, width, height, 0x000000, 0.9).setOrigin(0);
 
-    this.add.text(width / 2, height / 2 - 50, 'Game Over', {
+    const title = this.add.text(width / 2, height / 2 - 50, 'Game Over', {
       fontSize: '32px',
       color: '#fff'
     }).setOrigin(0.5);
 
     const finalScore = this.registry.get('score');
-    this.add.text(width / 2, height / 2, `Score: ${finalScore}`, {
+    const scoreText = this.add.text(width / 2, height / 2, `Score: ${finalScore}`, {
       fontFamily: 'SpaceGrotesk',
       fontSize: '24px',
       color: '#fff'
@@ -32,6 +32,16 @@ export default class GameOverScene extends Phaser.Scene {
       this.scene.stop('GameOverScene');
       this.scene.stop('GameScene');
       this.scene.start('GameScene');
+    });
+
+    const container = this.add.container(0, 0, [overlay, title, scoreText, tryAgainBtn]);
+    container.alpha = 0;
+
+    this.tweens.add({
+      targets: container,
+      alpha: 1,
+      duration: 500,
+      ease: 'Power2'
     });
   }
 }
