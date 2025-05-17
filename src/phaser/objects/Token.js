7627de1385055201d3c2373e8a2925fbb81160b3
Kokeh <kokehinc@gmail.com>
2025-05-17 15:40:50 +0100

    add token size reduction

--- src/phaser/objects/Token.js
+++ src/phaser/objects/Token.js
@@ -1,13 +1,13 @@
-export function spawnToken (scene) {
+export function spawnToken (scene, y = null) {
   const xPosition = scene.scale.width + 100;
   const minY = 50;
   const maxY = scene.scale.height - 50;
-  const yPosition = Phaser.Math.Between(minY, maxY);
+  const yPosition = y ?? Phaser.Math.Between(minY, maxY);
 
   const token = scene.physics.add.sprite(xPosition, yPosition, 'token');
   scene.tokens.add(token);
 
-  const targetWidth = 50;
+  const targetWidth = 40;
   const targetHeight = targetWidth * (token.height / token.width);
   token.displayWidth = targetWidth;
   token.displayHeight = targetHeight;
